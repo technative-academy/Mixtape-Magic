c4510d90b9a6eba55ad3cec2efc59edad74f809b
Helder Balbino <helderbalbino@googlemail.com>
2025-03-06 09:30:30 +0000

    add: wrap RouterProvider with Redux Provider in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import routes from './routes';
 import './assets/css/global.css';
 
@@ -7,6 +8,8 @@ const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById('root')).render(
 	<StrictMode>
-		<RouterProvider router={router} />
+		<Provider store={store}>
+			<RouterProvider router={router} />
+		</Provider>
 	</StrictMode>,
 );
