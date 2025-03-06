fe56ab83624be030e89315a1ddcb9699ab064aa4
Helder Balbino <helderbalbino@googlemail.com>
2025-03-06 21:56:06 +0000

    fix: update import path for authReducer in store.js

--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
-import authReducer from './features/auth/authSlice';
+
+import authReducer from './slices/authSlice';
 
 const store = configureStore({
 	reducer: {
