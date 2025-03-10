3262fad95cf3dd0f40c3d2311382491014f0d4a0
matt <m.young8@uni.brighton.ac.uk>
2025-03-10 15:54:46 +0000

    Fixing errors and indents

--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -1,13 +1,9 @@
 import Main from '../../components/ui/main/Main';
-import PlaylistList from '../../components/PlaylistList';
 
 const Home = () => {
     return (
       <Main />
     );
-    return (
-      <PlaylistList />
-    )
   };
   
 export default Home;
