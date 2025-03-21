95da6b088217f3b9242d81d00dd27171214e9912
Matt Young <99823652+matthewyoung94@users.noreply.github.com>
2025-03-21 12:18:25 +0000

    Update routes.jsx

--- src/routes.jsx
+++ src/routes.jsx
@@ -30,12 +30,12 @@ const routes = [
             },
             {
                 path: 'playlist/:id/edit/',
-                element: (
-                    <ProtectedRoute>
-                        <Edit />
-                    </ProtectedRoute>
-                ),
-                // element: <ProtectedRoute element={<Edit />} />,
+                // element: (
+                //     <ProtectedRoute>
+                //         <Edit />
+                //     </ProtectedRoute>
+                // ),
+                element: <ProtectedRoute element={<Edit />} />,
                 // element: <Edit />,
             },
 
