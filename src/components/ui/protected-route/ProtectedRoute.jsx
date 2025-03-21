bddc6cb934fe51bc35de349b9307f63ba55dec91
matt <m.young8@uni.brighton.ac.uk>
2025-03-21 11:51:57 +0000

    Changed protected route

--- src/components/ui/protected-route/ProtectedRoute.jsx
+++ src/components/ui/protected-route/ProtectedRoute.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
-import { Route } from 'react-router-dom'
+import React from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
 
-const ProtectedRoute = ({ component: Component, ...rest }) => {
-    return <Route {...rest} render={(props) => <Component {...props} />} />
-}
+const ProtectedRoute = ({ isAuthenticated }) => {
+    return isAuthenticated ? <Outlet /> : <Navigate to="/login" replace />;
+};
 
-export default ProtectedRoute
+export default ProtectedRoute;
+
+// import React from 'react'
+// import { Route } from 'react-router-dom'
+
+// const ProtectedRoute = ({ component: Component, ...rest }) => {
+//     return <Route {...rest} render={(props) => <Component {...props} />} />
+// }
+
+// export default ProtectedRoute
+
