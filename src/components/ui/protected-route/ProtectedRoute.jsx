097f09758879c7b23a5c4868e6322a33b712db4c
matt <m.young8@uni.brighton.ac.uk>
2025-03-21 12:08:12 +0000

    Revert "Changed protected route"
    This reverts commit bddc6cb934fe51bc35de349b9307f63ba55dec91.

--- src/components/ui/protected-route/ProtectedRoute.jsx
+++ src/components/ui/protected-route/ProtectedRoute.jsx
@@ -1,18 +1,8 @@
-import React from 'react';
-import { Navigate, Outlet } from 'react-router-dom';
+import React from 'react'
+import { Route } from 'react-router-dom'
 
-const ProtectedRoute = ({ isAuthenticated }) => {
-    return isAuthenticated ? <Outlet /> : <Navigate to="/login" replace />;
-};
+const ProtectedRoute = ({ component: Component, ...rest }) => {
+    return <Route {...rest} render={(props) => <Component {...props} />} />
+}
 
-export default ProtectedRoute;
-
-// import React from 'react'
-// import { Route } from 'react-router-dom'
-
-// const ProtectedRoute = ({ component: Component, ...rest }) => {
-//     return <Route {...rest} render={(props) => <Component {...props} />} />
-// }
-
-// export default ProtectedRoute
-
+export default ProtectedRoute
