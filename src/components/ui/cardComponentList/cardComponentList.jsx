d27fedefb5dc45ead4e1442c68797b7b40ea733a
ramseyediku <ramseyediku@protonmail.com>
2025-03-21 14:31:03 +0000

    Added functionality and styling for the 'my playlist' tab in the home page

--- src/components/ui/cardComponentList/cardComponentList.jsx
+++ src/components/ui/cardComponentList/cardComponentList.jsx
@@ -1,20 +1,9 @@
 import CardComponent from '../cardComponent/CardComponent'
 import LoadingComponent from '../loadingComponent/LoadingComponent'
-import { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { fetchPlaylists } from '../../../slices/playlistSlice'
 import styles from '../cardComponentList/cardComponentList.module.css'
 
-function CardComponentList() {
-    const dispatch = useDispatch()
-    const playlists = useSelector((state) => state.playlists.items)
-    const status = useSelector((state) => state.playlists.status)
-    const error = useSelector((state) => state.playlists.error)
-
-    useEffect(() => {
-        dispatch(fetchPlaylists())
-    }, [dispatch])
-
+function CardComponentList(props) {
+    const { playlists, status, error } = props
     return (
         <div className={styles.playlists}>
             {status === 'loading' && <LoadingComponent />}
--- src/views/my-playlists/MyPlaylists.jsx
+++ src/views/my-playlists/MyPlaylists.jsx
@@ -1,14 +1,32 @@
 import styles from '../../components/ui/main/main.module.css'
 import Search from '../../components/ui/seacrh/Search'
 import PlaylistNav from '../../components/ui/playlistNav/PlaylistNav'
+import CardComponentList from '../../components/ui/cardComponentList/cardComponentList'
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchMyPlaylists } from '../../slices/myPlaylistSlice'
 
 function MyPlaylists() {
+    const dispatch = useDispatch()
+    const myPlaylists = useSelector((state) => state.myPlaylists.items)
+    const status = useSelector((state) => state.myPlaylists.status)
+    const error = useSelector((state) => state.myPlaylists.error)
+
+    useEffect(() => {
+        dispatch(fetchMyPlaylists())
+    }, [dispatch])
+
     return (
         <main>
             <PlaylistNav />
             <section className={styles.main}>
                 <h1>My playlists</h1>
                 <Search />
+                <CardComponentList
+                    playlists={myPlaylists}
+                    status={status}
+                    error={error}
+                />
             </section>
         </main>
     )
